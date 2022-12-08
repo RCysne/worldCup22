3c95670dfcf8f901efd7ed1221666c12ef1b0a72
Ronaldo Cysne <cysne@MacBook-Ronaldo.local>
2022-12-08 08:54:23 -0300

    Atualização da responsividade

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function createGame (player1, hour, player2) {
   `
 }
 
+let delay = -0.4;
 function createCard (date, day, games) {
+  delay = delay + 0.4;
   return `
-  <div class="card">
+  <div class="card" style="animation-delay: ${delay}s">
       <h2>${date}<span>${day}</span></h2>
         <ul>
           ${games}
@@ -22,11 +24,11 @@ function createCard (date, day, games) {
 document.querySelector('#cards').innerHTML = 
 
     createCard('24/11', 'quinta', createGame('brasil', '16:00', 'serbia')) 
-+
+  +
     createCard('28/11', 'segunda', createGame('brasil', '13:00', 'switzerland'))
-+
+  +
     createCard('02/12', 'sexta', 
-    createGame('brasil', '16:00', 'cameroon') + 
-    createGame('portugal', '16:00', 'uruguay')
+      createGame('brasil', '16:00', 'cameroon') + 
+      createGame('portugal', '16:00', 'uruguay')
     )
     
